e6925c90a0cb837eb7487c432f42729168049fcf
zzephaniahh <zeph@umich.edu>
2021-01-10 15:40:37 -0500

    cluster set-up

--- SoLVe.py
+++ SoLVe.py
@@ -12,7 +12,7 @@ def main():
     if '--local' in sys.argv[:]:
         LOCAL = True
     CFG = c_ut.get_file_CFG(file_name)
-    c_ut.display_CFG(CFG, "raw_CFG")
+    # c_ut.display_CFG(CFG, "raw_CFG")
     # e_ut.get_equations(CFG)
     # CFG = o_ut.remove_redundant_nodes(CFG)
     # CFG = o_ut.remove_isolated_nodes(CFG)

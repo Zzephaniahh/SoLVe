cc4f33e36864c5bd9235996720248b954790f332
Zephaniah Hill <zeph@gl-login1.arc-ts.umich.edu>
2021-01-10 15:48:18 -0500

    cluster ready with global/local one-hots

--- process_locks_cluster.sh
+++ process_locks_cluster.sh
@@ -1,5 +1,8 @@
 for filename in cluster_test/*.c; do
-  { timeout -s SIGKILL 0.2s  time ./full_run.sh "$filename" ; } 2> "$filename"_out.txt
+  { timeout -s SIGKILL 0.2s  time ./full_run.sh "$filename" ; } 2> "$filename"_out_global.txt
 done
-/home/zeph/SoLVe
-/home/zeph/avr
+
+for filename in cluster_test/*.c; do
+  { timeout -s SIGKILL 0.2s  time ./full_run.sh "$filename" --local; } 2> "$filename"_out_local.txt
+done
+
